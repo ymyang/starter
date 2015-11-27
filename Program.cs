55e996b45462fa7663110774bf430d0a09b52161
ymyang <ymyang@163.com>
2015-11-27 11:44:06 +0800

    first

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace starter
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Process proc = new Process();
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.UseShellExecute = true;
+            proc.StartInfo.FileName = Path.Combine(Application.StartupPath, "yliyun.exe");
+
+            string arg = Path.Combine(Application.StartupPath, "app.nw");
+            foreach (string str in args)
+            {
+                arg = arg + " " + str;
+            }
+            proc.StartInfo.Arguments = arg;
+            // proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            proc.Start();
+        }
+    }
+}
